3df35cd2ff241c2cdf60b1d853e0738e626c5c24
nada142 <nadabouaziz142@gmail.com>
2024-12-17 11:15:24 +0100

    streamlit app

--- process_chunks.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-# List all chunk files
-for chunk in chunk_*.csv; do
-    # Run your model for each chunk and pass the chunk filename
-    echo "Processing $chunk..."
-    python notebooks/complexity_pred.py $chunk
-done
